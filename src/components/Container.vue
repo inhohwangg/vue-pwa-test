2ca7cca0476e7e102d51a314ee9d3f0992e35b0e
inhohwangg <sk1440sk@naver.com>
2022-11-14 23:52:59 +0900

    commit

---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Post :postdata="postdata[i]" v-for="(a, i) in postdata" :key="i" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  props: {
+    postdata: Array,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style>
+</style>
