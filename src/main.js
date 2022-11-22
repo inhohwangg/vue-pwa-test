91f1bd3996f9aa4f1c622e0da801cf2fa7656a23
inhohwangg <sk1440sk@naver.com>
2022-11-22 17:35:36 +0900

    pwa build

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import mitt from "mitt";
+import './registerServiceWorker'
 let emitter = mitt();
 let app = createApp(App);
 app.config.globalProperties.emitter = emitter;
